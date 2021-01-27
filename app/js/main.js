5eceacb3d27f5f5ea4108177d3a7bf2cd1c50cd7
Alex Volochai <smithloch00@gmail.com>
2021-01-27 13:52:52 +0200

    init

--- app/js/main.js
+++ app/js/main.js
@@ -13,5 +13,12 @@ $(function(){
         slidesToScroll: 4,
         asNavFor: '.header__slider',
     });
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt="">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt="">',
+    });
   
 });
